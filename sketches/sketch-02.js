869910e41bbb4ddf6455570ec0a90603f039e795
Agostina Tomalino <agostomalino@gmail.com>
2022-04-11 10:50:53 -0300

    Arcos

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,16 +1,19 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const degToRad = (degrees) =>{
-  return degrees / 180 * Math.PI;
-}
-
-const randomRange = (min, max) => {
-  return Math.random() * (max - min) + min;
-};
+//ESTA FUNCION CONVERTIA GRADOS EN RADIANTES
+// const degToRad = (degrees) =>{
+//   return degrees / 180 * Math.PI;
+// }
+// ESTA FUNCION RETORNABA UN NUMERO ALEATORIO
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min;
+// };
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -26,11 +29,11 @@ const sketch = () => {
 
     let x, y; 
 
-    const num = 30;
+    const num = 100;
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++){
-      const slice = degToRad(360/ num);
+      const slice = math.degToRad(360/ num);
       const angle = slice * i;
 
       x = radius * Math.sin(angle);
@@ -40,13 +43,23 @@ const sketch = () => {
       context.translate(cx, cy);
       context.translate(x, y);
       context.rotate(-angle);
-      context.scale(randomRange(1,3),Math.random() * 3);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
       
       context.beginPath();
-      context.rect(w * 0.5 ,h * 0.5 ,w ,h );
+      context.rect(w * 0.5 ,random.range(0, -h * 0.5),w ,h );
       context.fill();
       context.restore();
 
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+      
+      context.lineWidth = random.range(5,10);
+
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+      context.stroke();
+      context.restore();
     }
     
 
